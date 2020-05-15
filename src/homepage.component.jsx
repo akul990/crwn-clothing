638bbe0acb3b3be25d7b755eeb5983c621d026cd
Akul Bharti <akulbharti990@gmail.com>
2020-05-15 07:15:25 +0530

    broken down the hompage into small components and done all the Formating and styling of page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Direcotry from './components/directory/directory.component';
+
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <Direcotry />
+    </div>
+)
+
+export default HomePage;
