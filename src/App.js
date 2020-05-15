2c5b962542490b6d36affb7d5b0df048108b002c
Akul Bharti <akulbharti990@gmail.com>
2020-05-15 08:45:05 +0530

    third Push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import HomePage from './homepage.component';
+import  { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
-
 export default App;
